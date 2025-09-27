c8797a2390777d27d47b574741bca3da539a218a
Jose <jose.lopez.s@ucb.edu.bo>
2025-09-27 08:19:21 -0400

    feat: se agregaron 2 campos para la venta del dolar paralelo

--- app/src/main/java/com/calyrsoft/ucbp1/features/dollar/domain/model/DollarModel.kt
+++ app/src/main/java/com/calyrsoft/ucbp1/features/dollar/domain/model/DollarModel.kt
@@ -1,6 +1,9 @@
 package com.calyrsoft.ucbp1.features.dollar.domain.model
 
 data class DollarModel(
-    var dollarOfficial: String? = null,
-    var dollarParallel: String? = null
+    var dollarOfficialBuy: String? = null,
+    var dollarOfficialSell: String? = null,
+    var dollarParallelBuy: String? = null,
+    var dollarParallelSell: String? = null,
+    var lastUpdate: String? = null // fecha de actualización
 )
